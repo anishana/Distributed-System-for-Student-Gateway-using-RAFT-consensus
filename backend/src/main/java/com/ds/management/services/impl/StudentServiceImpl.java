54302ae323f666a084783e345905ad9c9edddcce
Atlas06 <anish.joshi06@gmail.com>
2022-03-05 01:45:32 -0500

    Create with replication completed

--- backend/src/main/java/com/ds/management/services/impl/StudentServiceImpl.java
+++ backend/src/main/java/com/ds/management/services/impl/StudentServiceImpl.java
@@ -6,6 +6,8 @@ import com.ds.management.repository.StudentRepository;
 import com.ds.management.services.MapperService;
 import com.ds.management.services.StudentService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -14,28 +16,30 @@ import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RequestCallback;
 import org.springframework.web.client.ResponseExtractor;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.logging.Logger;
 
 @Service
 public class StudentServiceImpl implements StudentService {
 
-    private final static Logger LOGGER = Logger.getLogger(StudentServiceImpl.class.getName());
-
-    @Value("serverUrl")
+    private final static Logger LOGGER = LoggerFactory.getLogger(StudentServiceImpl.class);
+    @Value("${server.url}")
     private String serverUrl;
 
-    @Value("studentCreateUrl")
+    @Value("${student.create.url}")
     private String studentCreate;
+
+    @Value("${student.delete.url}")
+    private String studentDelete;
+
+    @Value("${is.Master}")
+    private String isMaster;
 
     @Autowired
     private StudentRepository repository;
-
-    @Autowired
-    private Environment env;
 
     @Override
     public Student getStudentByEmail(String email) {
@@ -44,7 +48,6 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public List<Student> getStudents() {
-        String isMaster = env.getProperty("MASTER");
         if(isMaster.equalsIgnoreCase("yes")){
             LOGGER.info("is Master");
         } else {
@@ -53,9 +56,10 @@ public class StudentServiceImpl implements StudentService {
         return repository.findAll();
     }
 
-    RequestCallback requestCallback(final Student updated) {
+    RequestCallback requestCallback(StudentRequest updated) {
         return clientHttpRequest -> {
             ObjectMapper mapper = new ObjectMapper();
+            LOGGER.info("updated: "+updated.toString());
             mapper.writeValue(clientHttpRequest.getBody(), updated);
             clientHttpRequest.getHeaders().add(
                     HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
@@ -64,38 +68,66 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public Student createStudent(StudentRequest request) {
-        Student student= MapperService.mapStudentRequestToStudent(request);
-        String isMaster = env.getProperty("MASTER");
+        try{
+            Student student= MapperService.mapStudentRequestToStudent(request);
+            if(isMaster.equalsIgnoreCase("yes")){
+                LOGGER.info("createStudent.is Master");
+                AtomicInteger count= new AtomicInteger(1);
+                RestTemplate restTemplate = new RestTemplate();
+                ResponseExtractor<ClientHttpResponse> responseExtractor = response -> {
+                    LOGGER.info("createStudent"+response.toString());
+                    if(response.getStatusCode().value()==200){
+                        count.addAndGet(1);
+                        if(count.get()==3){
+                            repository.save(student);
+                        }
+                    }
+                    return response;
+                };
+
+                for(int i=1; i<3; i++){
+                    String resourceUrl= serverUrl+ Integer.toString(i)+ ":8080/"+ studentCreate;
+                    LOGGER.info("createStudent.resourceUrl "+resourceUrl);
+                    restTemplate.execute(resourceUrl, HttpMethod.POST, requestCallback(request), responseExtractor);
+                }
+            } else{
+                LOGGER.info("createStudent.is not Master");
+                repository.save(student);
+            }
+            return student;
+        } catch (Exception e) {
+            LOGGER.error("Exception: ",e);
+            throw e;
+        }
+
+    }
+
+    @Override
+    public void deleteStudent(String id){
         if(isMaster.equalsIgnoreCase("yes")){
-            LOGGER.info("is Master");
+            LOGGER.info("createStudent.is Master");
             AtomicInteger count= new AtomicInteger(1);
             RestTemplate restTemplate = new RestTemplate();
             ResponseExtractor<ClientHttpResponse> responseExtractor = response -> {
-                System.out.println(response.toString());
+                LOGGER.info("createStudent"+response.toString());
                 if(response.getStatusCode().value()==200){
                     count.addAndGet(1);
                     if(count.get()==3){
-                        repository.save(student);
+                        repository.deleteById(id);
                     }
                 }
                 return response;
             };
 
             for(int i=1; i<3; i++){
-                String resourceUrl= serverUrl+ Integer.toString(i)+ "/"+ studentCreate;
-                Student response = new Student();
-                restTemplate.execute(resourceUrl, HttpMethod.POST, requestCallback(response), responseExtractor);
+                String resourceUrl= serverUrl+ Integer.toString(i)+ ":8080/"+ studentDelete+"/"+id;
+                LOGGER.info("createStudent.resourceUrl "+resourceUrl);
+                restTemplate.execute(resourceUrl, HttpMethod.POST, null, responseExtractor);
             }
-            repository.save(student);
         } else{
-            LOGGER.info("is not Master");
-            repository.save(student);
+            LOGGER.info("createStudent.is not Master");
+            repository.deleteById(id);
         }
-        return student;
-    }
 
-    @Override
-    public void deleteStudent(String id){
-        repository.deleteById(id);
     }
 }
